aa46d9819c7df542226127cd3f787fda5f8d4f81
Busi <pietersonbusi@gmail.com>
2023-08-12 18:21:15 +0200

    changes made

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -10,6 +10,7 @@
 # This script will report the AWS resource usage
 #############################
 
+set -x
 
 # AWS S3
 # AWS EC2
@@ -17,13 +18,17 @@
 # AWS IAM users
 
 # list s3 buckets
+echo "Print list of s3 Buckets"
 aws s3 ls
 
 # list EC2 instances
-aws ec2 describe-instances
+echo "Print list of EC2 Instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
 
 # list lambda
+echo "Print List of lambda functions"
 aws lambda list-functions
 
 # list IAM users
+echo "Print list of Iam users"
 aws iam list-users
